275bc4e713db00c5e662e9d1ca02763fa4b0e3b1
Arghya Das <68916660+ArghyaDas21112001@users.noreply.github.com>
2021-09-28 22:56:14 +0530

    Update Real-Time-Clock.py

--- Real-Time-Clock.py
+++ Real-Time-Clock.py
@@ -1,19 +1,28 @@
 from time import strftime
 from tkinter import Label,Tk
+import datetime as date
 
 window=Tk()
 window.title("Real Time Clock")
-window.geometry("200x80")
+window.geometry("500x150")
 window.configure(bg="red")
-window.resizable(True,False)
+window.resizable(True,True)
 
 clock_label = Label(window,bg="red",fg="white",font=("Times",30,'bold'),relief='flat')
-clock_label.place(x=20,y=30)
+clock_label.place(x=20,y=50)
+date=date.datetime.now()
+
+format_date=f"{date:%a, %b %d %Y}"
+
+label=Label(window, text=format_date, font=("Times",30,'bold'))
+label.grid(padx=(0, 0), pady=(200, 0))
+label.pack()
 
 def updating_label():
     current_time=strftime('%H: %M: %S')
     clock_label.configure(text=current_time)
     clock_label.after(80,updating_label)
+    
 
 updating_label()
 window.mainloop()
